39d9ca636b5c415f84b40acab25428ea96d48cf3
Nick2324 <nicolas@Nick-PC>
2015-05-10 23:08:05 -0500

    Clases POJO eventos y servicio

---
+++ Prototipo/WebServices/SNA_DEPORTIVO_EJB/ejbModule/com/sna_deportivo/services/eventos/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ * 
+ * Contiene la logica de los servicios ofrecidos por concepto de Eventos Deportivos
+ * 
+ * @author Nicolas Mauricio Garcia Garzon
+ * @version 1.0
+ *
+ */
+package com.sna_deportivo.services.eventos;
--- Prototipo/WebServices/SNA_DEPORTIVO_EJB/ejbModule/com/sna_deportivo/utils/Relaciones.java
+++ Prototipo/WebServices/SNA_DEPORTIVO_EJB/ejbModule/com/sna_deportivo/utils/Relaciones.java
@@ -16,30 +16,57 @@ public class Relaciones {
 	//************************Eventos deportivos**************************
 	/**
 	 * 
-	 * Relación entre un {@link Entidades.#EVENTO} y su 
-	 * {@link Entidades.#USUARIO} creador
+	 * Relacion entre un {@link Entidades#EVENTODEPORTIVO} y su 
+	 * {@link Entidades#USUARIO} creador
 	 * 
 	 */
-	public static final String CREAEVENTO = "";	
+	public static final String CREAEVENTO = "R_CreaEvento";	
 	/**
 	 * 
-	 * Mapea la relación n-aria entre {@link Entidades.#DEPORTEEVENTO} y las siguientes:
+	 * Mapea la relacion n-aria entre {@link Entidades#DEPORTEEVENTO} y las siguientes:
 	 * 
-	 * - {@link Entidades.#DEPORTE}
-	 * - {@link Entidades.#GENERO}
-	 * - {@link Entidades.#DEPORTE}
-	 * - {@link Entidades.#EVENTODEPORTIVO}
-	 * - {@link Entidades.#UBICACION}
+	 * - {@link Entidades#DEPORTE}
+	 * - {@link Entidades#GENERO}
+	 * - {@link Entidades#DEPORTE}
+	 * - {@link Entidades#EVENTODEPORTIVO}
+	 * - {@link Entidades#UBICACION}
 	 * 
 	 */
-	public static final String EVENTODEDEPORTE = "";
+	public static final String EVENTODEDEPORTE = "R_EventoDeporte";
 	/**
 	 * 
-	 * Relación entre {@link Entidades.#USUARIO} y {@link Entidades.#DEPORTEEVENTO}
+	 * Relacion entre {@link Entidades#USUARIO} y {@link Entidades#DEPORTEEVENTO}
 	 * 
 	 */
-	public static final String PARTICIPANTEEVENTO = "";
+	public static final String PARTICIPANTEEVENTO = "R_ParticipaEvento";
+	/**
+	 * 
+	 * Relacion n-aria entre {@link Entidades#LUGARPRACTICA}, {@link Entidades#PAIS}
+	 *  y {@link Entidades#CIUDAD}
+	 * 
+	 */
 	
+	//****************************Ubicaciones******************************
+	public static final String  COMPLETAUBICACION = "R_CompletaUbicacion";
+	/**
+	 * 
+	 * Une a un Pais con su respectiva Ciudad
+	 * 
+	 */
+	public static final String PAISCIUDAD = "R_PaisCiudad";
+	/**
+	 * 
+	 * Relacion n-aria entre {@link Entidades#UBICACION}, {@link Entidades#DEPORTE} y 
+	 * {@link Entidades#GENERO}
+	 * 
+	 */
+	public static final String PRACTICADOEN = "R_PracticadoEn";
+	/**
+	 * 
+	 * Une una {@link Entidades#UBICACIONDEPORTE} con un {@link Entidades#USUARIO}
+	 * 
+	 */
+	public static final String UBICACIONUSUARIO = "R_UbicacionUsuario";
 	
 	private Relaciones(){}
 	
